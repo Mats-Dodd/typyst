85a11d46c5fbf722100eae7e73b17ef549a6e20e
Mats Dodd <mats.dodd12@gmail.com>
2024-12-26 01:16:05 +0100

    added padding to editor

--- src/features/editor/constants/constants.ts
+++ src/features/editor/constants/constants.ts
@@ -1,5 +1,8 @@
 export const INITIAL_CONTENT = `
 <p>
-  What will you write?
+  BERT was released in 2018 (millennia ago in AI-years!) and yet it’s still widely used today: in fact, it’s currently the second most downloaded model on the HuggingFace hub, with more than 68 million monthly downloads, only second to another encoder model fine-tuned for retrieval. That’s because its encoder-only architecture makes it ideal for the kinds of real-world problems that come up every day, like retrieval (such as for RAG), classification (such as content moderation), and entity extraction (such as for privacy and regulatory compliance).
+Finally, 6 years later, we have a replacement! Today, we at Answer.AI and LightOn (and friends!) are releasing ModernBERT. ModernBERT is a new model series that is a Pareto improvement over BERT and its younger siblings across both speed and accuracy. This model takes dozens of advances from recent years of work on large language models (LLMs), and applies them to a BERT-style model, including updates to the architecture and the training process.
+
+I know for a fact that its proven to be more efficient and accurate. It demands of high standards and asks performance benchmarks that you dont ever think you would ever need to use. Who knows its potential impact on future language model
 </p>
 `; 
