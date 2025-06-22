f33199375ce3d56da53de79201f450efa5cb768d
Mats Dodd <mats.dodd12@gmail.com>
2025-06-21 22:24:53 -0700

    new refresh collections

--- apps/web/src/routes/(app)/notes/sidebar.svelte
+++ apps/web/src/routes/(app)/notes/sidebar.svelte
@@ -2,11 +2,11 @@
 	import { fetchCollectionEntries } from '@/api/collection';
 	import { createFolder } from '@/api/folders';
 	import { createNote, openNote } from '@/api/notes';
+	import { refreshCollection } from '@/api/store-helpers';
 	import Icon from '@/components/shared/icon.svelte';
 	import Shortcut from '@/components/shared/shortcut.svelte';
 	import Tooltip from '@/components/shared/tooltip.svelte';
 	import { SHORTCUTS } from '@/constants';
-	import { pgClient } from '@/database/client';
 	import {
 		activeFile,
 		collection,
@@ -37,16 +37,6 @@
 	let entries: FileEntry[] = [];
 	let folderToggleState: 'collapse' | 'expand';
 	let toggleFolderStates: () => void;
-	let stopWatching: () => void;
-
-	// Watch for changes in the collection
-	async function watchCollection() {
-		const dbWatcher = await pgClient.live.query(`SELECT * FROM entry`, [], async () => {
-			await fetchCollectionEntries($collection);
-		});
-
-		return dbWatcher.unsubscribe;
-	}
 
 	const stopWatchingStore = collectionEntries.subscribe((value) => {
 		entries = value;
@@ -64,12 +54,12 @@
 		} else {
 			activeFile.set(null);
 		}
-
-		if (value) {
-			if (stopWatching) stopWatching();
-			stopWatching = await watchCollection();
-		}
 	});
+
+	// Refresh collection when it changes
+	$: if ($collection) {
+		refreshCollection($collection);
+	}
 
 	collectionSearchActive.subscribe((value) => {
 		// Should focus inputs when search is active
@@ -167,7 +157,6 @@
 	}
 
 	onDestroy(() => {
-		if (stopWatching) stopWatching();
 		stopWatchingStore();
 		stopWatchingCollectionStore();
 	});
